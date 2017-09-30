c7ed2a3dfc37254ef7630a885f937a415efde9ff
Lyall Morrison <Lyall.Morrison@gmail.com>
2017-09-30 21:51:29 +1300

    Moved connection string into web.config

--- refactor-me/Models/Helpers.cs
+++ refactor-me/Models/Helpers.cs
@@ -1,15 +1,13 @@
-﻿using System.Data.SqlClient;
-using System.Web;
+﻿using System.Configuration;
+using System.Data.SqlClient;
 
 namespace refactor_me.Models
 {
     public class Helpers
     {
-        private const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={DataDirectory}\Database.mdf;Integrated Security=True";
-
         public static SqlConnection NewConnection()
         {
-            var connstr = ConnectionString.Replace("{DataDirectory}", HttpContext.Current.Server.MapPath("~/App_Data"));
+            var connstr = ConfigurationManager.ConnectionStrings["EntityStorage"].ConnectionString;
             return new SqlConnection(connstr);
         }
     }
